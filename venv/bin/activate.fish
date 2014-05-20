ac6e9c3505f7ced1a81e825bbb48c1aea81c942b
MariellaP123 <mmp344@gmail.com>
2014-05-20 15:15:55 -0400

    Heroku stuff up

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/mariellapaulino/gitrepos/HearingResources/venv"
+set -gx VIRTUAL_ENV "/home/mariellapaulino/gitrepos/TheHearingProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
